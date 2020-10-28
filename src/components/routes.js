84646def0ff13f71e574cc867f1c4d754645c067
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-28 16:41:10 -0700

    Setup boilerplate for routes

---
+++ src/components/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+
+export const Routes = () => {
+  return (
+    <Router>
+      <Routes path="/" component={} />
+      <Routes path="/" component={} />
+      <Routes path="/" component={} />
+      <Routes path="/" component={} />
+    </Router>
+  );
+};
