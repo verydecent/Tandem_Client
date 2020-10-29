79bdcf94132db5b46ffce11fe78368712ac3b9da
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-28 20:13:20 -0700

    add navigation

--- src/RouteWrapper.js
+++ src/RouteWrapper.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navigation from "./components/navigation";
 import LeaderBoard from "./components/leaderboard";
 import Home from "./components/home";
 import Trivia from "./components/trivia";
@@ -7,6 +8,7 @@ import Trivia from "./components/trivia";
 export const RouteWrapper = () => {
   return (
     <Router>
+      <Route path="/" component={Navigation} />
       <Route exact path="/" component={Home} />
       <Route path="/app" component={Trivia} />
       <Route path="/leaderboard" component={LeaderBoard} />
