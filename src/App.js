fe9ba286a6416f6fdecbfa096c8040ef0182f68a
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-28 17:00:00 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import "./App.css";
 import { RouteWrapper } from "./routes";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Header</header>
-      <RouteWrapper />
-    </div>
-  );
+  return <RouteWrapper />;
 }
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import LeaderBoard from "../components/leaderboard";
-import Home from "../components/home";
-import Trivia from "../components/trivia";
+import LeaderBoard from "./components/leaderboard";
+import Home from "./components/home";
+import Trivia from "./components/trivia";
 
 export const RouteWrapper = () => {
   return (
     <Router>
       <Route path="/" component={Home} />
-      <Route path="/" component={Trivia} />
-      <Route path="/" component={LeaderBoard} />
+      <Route exact path="app" component={Trivia} />
+      <Route path="app/leaderboard" component={LeaderBoard} />
     </Router>
   );
 };
