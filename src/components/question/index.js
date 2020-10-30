45495c1f06ed9e2a6b4a1747f1d9cf3c21bf8a8b
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-30 16:35:13 -0700

    update

--- src/components/question/index.js
+++ src/components/question/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Question = ({ id, question }) => (
   <div className="question">
     <h1>Question {id}</h1>
-    <h1>{question}</h1>
+    <h2>{question}</h2>
   </div>
 );
 
