1f67b3c6a6697d13769af0fb986b1cf6efc669f3
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-30 11:48:22 -0700

    Add modal open status from global state and import modals

--- src/components/trivia/index.js
+++ src/components/trivia/index.js
@@ -3,12 +3,17 @@ import Navigation from "../navigation";
 import Card from "../card";
 import { withRouter, Link } from "react-router-dom";
 import { connect } from "react-redux";
+import Incorrect from "../modals/incorrect";
+import Correct from "../modals/correct";
 
-const Trivia = ({ cards, match, history }) => {
+const Trivia = ({ cards, match, isCorrectModalOpen, isIncorrectModalOpen }) => {
   const { id } = match.params;
 
   return (
     <div className="trivia">
+      {isCorrectModalOpen && Correct}
+      {isIncorrectModalOpen && Incorrect}
+
       <Navigation />
       <Card card={cards[id]} />
       <Link to={id < 21 ? `/trivia/${parseInt(id) + 1}` : "/leaderboard"}>
@@ -19,7 +24,13 @@ const Trivia = ({ cards, match, history }) => {
 };
 
 const TriviaWithRouter = withRouter(Trivia);
+
 const mapDispatch = (dispatch) => ({});
-const mapState = (state) => ({ cards: state.cards });
+
+const mapState = (state) => ({
+  cards: state.cards,
+  isCorrectModalOpen: state.isCorrectModalOpen,
+  isIncorrectModalOpen: state.isIncorrectModalOpen,
+});
 
 export default connect(mapState, mapDispatch)(TriviaWithRouter);
