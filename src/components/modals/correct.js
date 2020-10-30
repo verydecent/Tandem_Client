02a250d0d6a700d14e66a6098083a72cd3ac2fab
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-30 11:45:05 -0700

    modal boilerplate

---
+++ src/components/modals/correct.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Correct = () => {
+  return (
+    <div className="modal">
+      <div className="modal-overlay"></div>
+      <div className="modal-content">Correct</div>
+    </div>
+  );
+};
+
+export default Correct;
---
+++ src/components/modals/incorrect.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Incorrect = () => {
+  return (
+    <div className="modal">
+      <div className="modal-overlay"></div>
+      <div className="modal-content">Incorrect</div>
+    </div>
+  );
+};
+
+export default Incorrect;
