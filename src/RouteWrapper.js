ebae1f75d525c0dba33658a4b143bdfb8ed96001
Wonjae Hwang <wnjhwng@gmail.com>
2020-10-29 23:21:00 -0700

    remove as

--- src/RouteWrapper.js
+++ src/RouteWrapper.js
@@ -1,23 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import LeaderBoard from "./components/leaderboard";
 import Home from "./components/home";
 import Trivia from "./components/trivia";
 import "./App.css";
-import { connect } from "react-redux";
 
 const RouteWrapper = ({ cards }) => {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="max-width-container">
         <Route exact path="/" component={Home} />
         <Route path="/trivia/:id" component={Trivia} />
         <Route path="/leaderboard" component={LeaderBoard} />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 };
 
-const mapState = (state) => ({ cards: state.cards });
-
-export default connect(mapState)(RouteWrapper);
+export default RouteWrapper;
