c499db4fcb4c2a58b72acacedf820278c8a9546e
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-01 23:40:41 -0800

    last update

--- src/components/leaderboard/index.js
+++ src/components/leaderboard/index.js
@@ -1,17 +1,33 @@
 import React from "react";
 import Navigation from "../navigation";
 import Confetti from "react-confetti";
+import { connect } from "react-redux";
 
-const LeaderBoard = () => {
+const LeaderBoard = ({ points }) => {
   return (
     <div className="max-width-container">
-      {/* <Confetti /> */}
       <div className="leaderboard">
+        <Confetti />
         <Navigation />
-        Thank you for playing!!!
+        <div>
+          <div className="leaderboard-text">You scored {points} points!</div>
+          <span className="leaderboard-text">
+            Thank you for playing, if you have time please check out more of my
+            other works&nbsp;
+          </span>
+          <span>
+            <a id="leaderboard-link" href="https://www.wonjaehwang.com">
+              here
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   );
 };
 
-export default LeaderBoard;
+const mapState = (state) => ({
+  points: state.points,
+});
+
+export default connect(mapState)(LeaderBoard);
