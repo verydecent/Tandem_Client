7d435c5d1440d2a9ba465b10b8f9e79505bdbc35
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-01 22:21:39 -0800

    Convert functional component to class

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -9,47 +9,56 @@ import Correct from "../modals/correct";
 import Timeup from "../modals/timeup";
 import { connect } from "react-redux";
 
-const Card = ({
-  // values
-  cards,
-  match,
-  isCorrectModalOpen,
-  isIncorrectModalOpen,
-  isTimeupModalOpen,
-}) => {
-  const id = parseInt(match.params.id);
-  const { question, incorrect, correct } = cards[id];
-  const choices = shuffle([...incorrect, correct]);
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  return (
-    <div className="card">
-      {isCorrectModalOpen && <Correct />}
-      {isIncorrectModalOpen && <Incorrect />}
-      {isTimeupModalOpen && <Timeup />}
+  componentDidUpdate() {}
 
-      <div className="question-section">
-        <Question id={id} question={question} />
+  render() {
+    const id = parseInt(match.params.id);
+    const { question, incorrect, correct } = cards[id];
+    const choices = shuffle([...incorrect, correct]);
+    const {
+      cards,
+      match,
+      isCorrectModalOpen,
+      isIncorrectModalOpen,
+      isTimeupModalOpen,
+    } = this.props;
+
+    return (
+      <div className="card">
+        {isCorrectModalOpen && <Correct />}
+        {isIncorrectModalOpen && <Incorrect />}
+        {isTimeupModalOpen && <Timeup />}
+
+        <div className="question-section">
+          <Question id={id} question={question} />
+        </div>
+        <div className="multiple-choice-section">
+          {choices.map((choice, index) => (
+            <Choice
+              key={shortid.generate()}
+              choice={choice}
+              correct={correct}
+              index={index}
+            />
+          ))}
+        </div>
+        {/*
+          Makes reques to check
+          Returns 0 or 1
+          if 0 then tells you wrong message
+          if 1 then got it right
+          both will present next question button  
+        */}
       </div>
-      <div className="multiple-choice-section">
-        {choices.map((choice, index) => (
-          <Choice
-            key={shortid.generate()}
-            choice={choice}
-            correct={correct}
-            index={index}
-          />
-        ))}
-      </div>
-      {/*
-        Makes reques to check
-        Returns 0 or 1
-        if 0 then tells you wrong message
-        if 1 then got it right
-        both will present next question button  
-      */}
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapDispatch = (dispatch) => ({});
 
