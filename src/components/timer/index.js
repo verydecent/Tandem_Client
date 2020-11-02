17a00dcb0fdebb1c39c27922d6030cc12c97a3f5
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-01 16:59:57 -0800

    update component

---
+++ src/components/timer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Timer = () => {
+  return <div className="timer">Timer Component</div>;
+};
+
+export default Timer;
--- src/components/usercard/index.js
+++ src/components/usercard/index.js
@@ -1,16 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const UserCard = ({ username, points }) => {
-  return (
-    <div className="usercard">
-      <h1 className="">Username: {username}</h1>
-      <h1 className="">
-        Points: <span className="underline">{points}</span>
-      </h1>
-    </div>
-  );
-};
+// const UserCard = ({ username, points }) => {
+//   return (
+//     <div className="usercard">
+//       <h1 className="">
+//         Points: <span className="underline">{points}</span>
+//         <p>{Timer()}</p>
+//       </h1>
+//       <h1 className="">Username: {username}</h1>
+//     </div>
+//   );
+// };
+
+class UserCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { username, points } = this.props;
+
+    return (
+      <div className="usercard">
+        <h1 className="usercard-name oj">{username}</h1>
+        <h1 className="usercard-name">
+          <span className="underline">{points}</span> Points
+        </h1>
+      </div>
+    );
+  }
+}
 
 const mapState = (state) => ({
   username: state.username,
