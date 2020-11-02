957f53999862da56fa5ad548acd73f5197222eca
Wonjae Hwang <wnjhwng@gmail.com>
2020-11-01 17:05:04 -0800

    Create Timer

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
-const Timer = () => {
-  return <div className="timer">Timer Component</div>;
-};
+const Timer = () => (
+  <CountdownCircleTimer
+    isPlaying
+    duration={10}
+    colors={[
+      ["#004777", 0.33],
+      ["#F7B801", 0.33],
+      ["#A30000", 0.33],
+    ]}
+  >
+    {({ remainingTime }) => remainingTime}
+  </CountdownCircleTimer>
+);
 
 export default Timer;
